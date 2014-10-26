28a42a3bf19d02893992e51191531cc4d71f7c3f
Javier Acero <j4cegu@gmail.com>
2014-10-26 13:25:09 +0100

    Use subject instead of described_class.new
    You can use another useful alias in RSpec tests: subject. The value of
    subject defaults to a new instance of the described class constructed by
    calling .new on the described class without any parameters.
    
    You can override it for your convenience if you need to:
    
    describe MyClass do
      subject { MyClass.new(param1, param2) }
      let(:param1) { 'foo' }
      let(:param2) { 'bar' }
    
      #...
    end
    
    Those let sentences are a common way of extracting values to be reused
    between specs. The first time param1 is called from the spec it will
    execute the bloc and memoize the value so it's never run again.

--- spec/a/b/my_class_spec.rb
+++ spec/a/b/my_class_spec.rb
@@ -3,8 +3,8 @@ require 'a/b/my_class'
 describe A::B::MyClass do
 
   it "It loads B class if includes A::B" do
-    expect(described_class.new.greet).not_to match /^Hi/
-    expect(described_class.new.greet).to match /^F/
+    expect(subject.greet).not_to match /^Hi/
+    expect(subject.greet).to match /^F/
   end
 
 end
--- spec/a/my_class_spec.rb
+++ spec/a/my_class_spec.rb
@@ -3,8 +3,8 @@ require 'a/my_class'
 describe A::MyClass do
 
   it "It loads A class if no module is specified" do
-    expect(described_class.new.greet).to match /^Hi/
-    expect(described_class.new.greet).to_not match /^F/
+    expect(subject.greet).to match /^Hi/
+    expect(subject.greet).to_not match /^F/
   end
 
 end
